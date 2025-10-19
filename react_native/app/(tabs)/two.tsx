2c3914281e3bf39bb0efbf7f3bbcbbb59e3a60ce
Sr-Aileron <faucilleriziere@outlook.jp>
2025-10-19 15:27:55 +0900

    .

--- react_native/app/(tabs)/two.tsx
+++ react_native/app/(tabs)/two.tsx
@@ -1,11 +1,7 @@
 import { Text, View } from 'tamagui'
+import MyUnityScreen from '../../components/MyUnityScreen'
 
 export default function TabTwoScreen() {
-  return (
-    <View flex={1} items="center" justify="center" bg="$background">
-      <Text fontSize={20} color="$blue10">
-        Tab Two
-      </Text>
-    </View>
-  )
+  return <MyUnityScreen/>
+
 }
